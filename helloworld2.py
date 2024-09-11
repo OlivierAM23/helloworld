e46f28e0371b7509ea78d9bf3d251cb95d46f586
OlivierAM23 <538287@student.fontys.nl>
2024-09-11 09:50:27 +0200

    removal of dinner (i ate it)

--- helloworld2.py
+++ helloworld2.py
@@ -5,8 +5,3 @@ print('this dog sucks')
 print('this dog is so lame')
 print('the dog has been roasted enough')
 print('very good ending')
-
-print('infinite sadness')
-print('i ate the polar express')
-print('humgry')
-print('dinner')
